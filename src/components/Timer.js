29033819f573c20416fa0ac9029e6a9646fc29a0
ngvuong <vminguyen@ucdavis.edu>
2021-12-12 04:12:37 -0800

    Update Timer to show seconds and minutes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 function Timer() {
-  const [time, setTime] = useState(
-    new Date().toLocaleTimeString().substring(0, 7)
-  );
+  const [time, setTime] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
 
   useEffect(() => {
     const timeId = setTimeout(() => {
-      setTime(new Date().toLocaleTimeString().substring(0, 7));
+      setTime(time + 1);
+      setSeconds(time % 60);
+      if (minutes !== Math.floor(time / 60)) {
+        setMinutes(minutes + 1);
+      }
     }, 1000);
 
     return () => clearTimeout(timeId);
-  }, [time]);
+  }, [time, seconds, minutes]);
 
   return <StyledTimer>{time}</StyledTimer>;
 }
