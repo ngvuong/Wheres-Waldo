9087bd578fe69400560a6968b8d8798d9773ca47
ngvuong <vminguyen@ucdavis.edu>
2021-12-14 21:34:50 -0800

    Update Leaderboard to show times and names

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import {
   getFirestore,
@@ -9,8 +9,7 @@ import {
 } from "firebase/firestore";
 
 function Leaderboard({ onClose }) {
-  const recordRef = useRef([]);
-  let records = [];
+  const [records, setRecords] = useState([]);
   useEffect(() => {
     const getRecords = async () => {
       const searchQuery = query(
@@ -20,24 +19,31 @@ function Leaderboard({ onClose }) {
 
       const querySnapshot = await getDocs(searchQuery);
       querySnapshot.forEach((doc) => {
-        recordRef.current.push(doc.data());
-        console.log(recordRef.current);
+        setRecords((prevRecords) => {
+          const record = doc.data();
+          record.id = doc.id;
+          return [...prevRecords, record];
+        });
       });
     };
     getRecords();
   }, []);
 
-  useEffect(() => {
-    records = recordRef.current.map((doc) => {
-      return <div>hello</div>;
-    });
-  }, []);
+  const times = records.map((doc, i) => {
+    return (
+      <div key={doc.id}>
+        <div>{i + 1}.</div>
+        <div>{doc.name.toUpperCase()}</div>
+        <div>{doc.time}</div>
+      </div>
+    );
+  });
 
   return (
     <StyledLeaderboard>
       <span onClick={onClose}>&#10006;</span>
       <h2>Leaderboard</h2>
-      {records}
+      {times}
     </StyledLeaderboard>
   );
 }
@@ -49,6 +55,7 @@ const StyledLeaderboard = styled.div`
   width: 90vw;
   height: 90vh;
   text-align: center;
+  color: black;
   background-color: ${({ theme }) => theme.colors.leaderboard};
 
   h2 {
@@ -61,5 +68,11 @@ const StyledLeaderboard = styled.div`
     right: 0;
     top: 0;
     cursor: pointer;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    border-bottom: 1px solid black;
   }
 `;
